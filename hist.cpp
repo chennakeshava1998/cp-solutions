64b309f962589af99f04ce1aee74dbf67d2be11b
chennakeshava1998 <16co108.keshava@nitk.edu.in>
2018-05-13 22:41:53 +0530

    add solution for histogram problem

---
+++ hist.cpp
@@ -0,0 +1,69 @@
+// problem - http://www.spoj.com/problems/HISTOGRA/
+// explanation - https://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n, x;
+	cin >> n;
+	while(n != 0)
+	{
+		vector<int>v;
+
+		while(n--)
+		{
+			cin >> x;
+			v.push_back(x);
+		}
+		stack<int>s; 
+		/* push only indices into the stack; 
+		* 1. No need to again search for the index within the input array. index of top element of stack is required to find the area 
+		* 2. If identical values are present in the array, confusions can be avoided
+		*/
+
+		int i = 0, l = v.size(), max_area = 0, tmp_area;
+		// use l instead of v.size(), not much of gains here anyway, because v.size() takes constant time. 
+		while(i < l)
+		{
+			if(s.empty() || s.top() < v[i])
+				s.push(i++); // push i and later increment it! (i gets incremented only at this point.)
+			else
+			{
+				int top = s.top();
+				s.pop();
+
+				// calculation of current area:
+				if(s.empty())
+					tmp_area = v[top] * i;
+				else
+					tmp_area = v[top] * (i - s.top() - 1);
+
+				if(max_area < tmp_area)
+					max_area = tmp_area;
+			}
+		}
+
+		while(!s.empty())
+		{
+			int top = s.top();
+			s.pop();
+
+			// calculation of current area:
+			if(s.empty())
+				tmp_area = v[top] * i;
+			else
+				tmp_area = v[top] * (i - s.top() - 1);
+
+			if(max_area < tmp_area)
+				max_area = tmp_area;
+		}
+
+		cout << max_area << "\n";
+
+		cin >> n;
+	}
+
+	return 0;
+}

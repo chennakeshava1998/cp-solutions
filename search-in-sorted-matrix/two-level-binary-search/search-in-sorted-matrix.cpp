ce3b846b04a156db8b72da86b3cb84afaf83b4b2
Chenna Keshava B S <16co108.keshava@nitk.edu.in>
2018-05-16 00:01:51 +0530

    add comments, 2-level BS is wrong

--- search-in-sorted-matrix/two-level-binary-search/search-in-sorted-matrix.cpp
+++ search-in-sorted-matrix/two-level-binary-search/search-in-sorted-matrix.cpp
@@ -1,7 +1,23 @@
 // https://practice.geeksforgeeks.org/problems/search-in-a-matrix/0
-// Time Complexity: O(log(n * log(m) ) )
+// Time Complexity: O(log(n) * log(m) )
 // n = # of rows; m = # of columns
 
+/*
+ * This solution does not work because I am only performing binary-search(BS) on the first column elements,
+ * and then trying to use them as indexes for the second-level BS on that particular row.
+ * All the rows and columns are sorted, but that does not imply a correlation between the elements of rows and cols
+ * An counter-example:
+
+ 1
+ 3 3
+ 1 1 5
+ 2 3 7
+ 4 6 8
+ 5 (element to be searched)
+ 
+ this code will perform BS only the first column and conclude that 5 is not present. This is a mistake.
+ * 
+ */
 
 #include<bits/stdc++.h>
 using namespace std;

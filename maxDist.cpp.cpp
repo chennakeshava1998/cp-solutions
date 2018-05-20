8e55ba5dbf948b03aaa43e3d50bdc88c0eb84c9f
chennakeshava1998 <16co108.keshava@nitk.edu.in>
2018-05-20 20:19:43 +0530

    Update maxDist.cpp.cpp

--- maxDist.cpp.cpp
+++ maxDist.cpp.cpp
@@ -12,31 +12,35 @@ int main()
 		v.push_back(x);
 	}
 
-	vector<int>leftMin; // holds the minimum from v[0] to v[i]
+	vector<int>leftMin(v.size()); // holds the minimum from v[0] to v[i]
+	// initialise the size of leftMin vector, otherwise segmentation fault is caused.
 	leftMin[0] = v[0];
 	for(i=1;i<v.size();++i)
 	{
 		leftMin[i] = min(leftMin[i-1], v[i]);	
 	}
+	
+	/* Debug Info:
 	cout << "left min array\n";
 	
 	for(i=0;i<v.size();++i)
 		cout << leftMin[i] << " ";
 	cout << "\n";
-	int  dist = -1;
+	*/
+	int  dist = -1; // maximum distance of separation
 
 	i = v.size()-1;
 	j = v.size()-1;
 
 	while(i>=0 && j>=0)
 	{
-		if(v[j]>leftMin[i])
+		if(v[j]>leftMin[i])  // comparing v[j] with the range-minimum-minimum within v[0]...v[i]
 		{
 			dist = max(dist, j - i);
-			--i; // move the left pointer backwards
+			--i; // move the left pointer to the back(left) of the array
 		}
 		else
-			--j;
+			--j; // move the right pointer to the back of the array
 	}
 
 	cout << dist << "\n";		

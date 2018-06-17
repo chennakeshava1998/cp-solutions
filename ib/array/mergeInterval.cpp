74cd7130afb81ebfb0e0d801e79bb71edfc4c760
Chenna Keshava B S <16co108.keshava@nitk.edu.in>
2018-06-17 22:23:28 +0530

    syntax errors

--- ib/array/mergeInterval.cpp
+++ ib/array/mergeInterval.cpp
@@ -7,23 +7,17 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
-// O(n^2) answer
-
-bool mergeable(Interval a, Interval b);
-bool mergeable(Interval a, Interval b)
-{
-
-    if (a.start > b.end || a.end < b.start)
-        return 0;
-
-    return 1;
-}
 
 vector<Interval> Solution::merge(vector<Interval> &A)
 {
-    set<Interval> s;
+    // sorting the array
+    int i, j;
     for (i = 0; i < A.size(); ++i)
-        s.insert(A[i]);
-
-    
+    {
+        for (j = i + 1; j < A.size(); ++j)
+        {
+            if (A[i].start > A[j].start)
+                swap(A[i], A[j]);
+        }
+    }
 }

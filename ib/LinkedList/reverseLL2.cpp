d877109472e95d0748b338bd29c25bb0ea9da344
Chenna Keshava B S <16co108.keshava@nitk.edu.in>
2018-07-14 22:06:13 +0530

    add approach-2, change node links

--- ib/LinkedList/reverseLL2.cpp
+++ ib/LinkedList/reverseLL2.cpp
@@ -7,6 +7,7 @@
  * };
  */
 
+// Uses Stack, 2 passes
 ListNode *movePointer(ListNode *A, int B);
 void func(ListNode *A, int B);
 
@@ -48,6 +49,61 @@ ListNode *Solution::reverseBetween(ListNode *A, int B, int C)
     --B;
     --C;
     ListNode *ptr = movePointer(A, B);
+
+    func(ptr, C - B);
+
+    return A;
+}
+
+// ====================================================
+// Approach-2: Change the links
+
+ListNode *left_kick, *right_kick, *first_left_parent;
+ListNode *movePointer(ListNode *A, int B);
+void func(ListNode *A, int B);
+
+ListNode *movePointer(ListNode *A, int B)
+{
+    // Index is 1-based, according to the question
+    ListNode *ptr = A;
+    while (B--)
+    {
+        left_kick = ptr;
+        ptr = ptr->next;
+    }
+
+    return ptr;
+}
+
+void func(ListNode *A, int B)
+{
+    ++B;
+    ListNode *ptr = A;
+    while (B--)
+    {
+        right_kick = ptr->next;
+        ptr->next = left_kick;
+        left_kick = ptr;
+        ptr = right_kick;
+    }
+
+    first_left_parent->next = left_kick;
+    A->next = ptr;
+}
+
+ListNode *Solution::reverseBetween(ListNode *A, int B, int C)
+{
+    // Handling this case separately, if B==C, then left_kick's value will not be updated.
+    if (B == C)
+        return A;
+    // Working with 0-based indices
+    --B;
+    --C;
+    left_kick = NULL;
+    right_kick = NULL;
+    ListNode *ptr = movePointer(A, B);
+
+    first_left_parent = left_kick;
 
     func(ptr, C - B);
 

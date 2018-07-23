12f399eaa40a099c72b72de6a8c36e694b59ab98
Chenna Keshava B S <16co108.keshava@nitk.edu.in>
2018-07-23 09:05:41 +0530

    TLE

--- ib/strings/PowerOf2.cpp
+++ ib/strings/PowerOf2.cpp
@@ -1,26 +1,58 @@
 // not completed - https://www.interviewbit.com/problems/power-of-2/
 
+// linearly parses through all strings from the given number to 1, dividing by 2 in each iteration. TLE.
 #include <bits/stdc++.h>
 using namespace std;
 
-bool odd, carry; // True, if the div is odd
+bool carry; // True, if the div is odd
 
 string func(string A);
-string divide(string div);
+string divide(char div);
+string clearLeadingZeros(string);
+bool isPowerOfTwo(string);
+
+bool isPowerOfTwo(string A)
+{
+    carry = 0;
+    if (A == "1" || A == "0")
+        return 0;
+
+    while (1)
+    {
+        if (A == "1")
+            return 1;
+
+        if (A != "1" && ((A[A.size() - 1] - 48) & 1))
+            return 0;
+
+        A = func(A);
+    }
+}
+
+string clearLeadingZeros(string A)
+{
+    int i = 0;
+    while (A[i] == '0')
+        ++i;
+
+    string ans = A.substr(i);
+    return ans;
+}
 
 string func(string A)
 {
-    char m = A[A.size()-10];
-    if((m-48)&1 && A!="1") // last bit is odd, so it cannot be a power of 2
+    string ans;
+    for (int i = 0; i < A.size(); ++i)
     {
-        odd=1;
-        return "";
+        ans += divide(A[i]);
     }
 
-   
+    ans = clearLeadingZeros(ans);
+
+    return ans;
 }
 
-string divide(string div)
+string divide(char div)
 {
     string temp;
     if (carry)
@@ -30,17 +62,16 @@ string divide(string div)
     }
 
     temp += div;
+
+    // does a carry occur for the current division?
+    if ((temp[temp.size() - 1] - 48) & 1) // is the last digit odd?
+        carry = 1;                        // useful for the next operation
+
     stringstream ss(temp);
     int x = 0;
     ss >> x;
 
     x /= 2;
-
-    if (x & 1)
-    {
-        odd = 1;
-        return "";
-    }
 
     // Clearing the contents of the stringstream. clear() method is inherited in the ios class for clearing the error bit of the streams. It's not used for emptying the contents. If EOF file is reached, then error bit will be set to 1 in such streams. But clear() method will reset it back to 0.
 
@@ -55,13 +86,12 @@ string divide(string div)
 
 int main()
 {
-
-    odd = 0;
     carry = 0;
     string inp;
     cin >> inp;
 
-    cout << divide(inp, 0) << endl;
+    cout << isPowerOfTwo(inp) << endl;
+    // cout << carry << endl;
 
     return 0;
 }
